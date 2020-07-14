bb7f34710bfe9277b2498c0abc884f58419e18e3
Braden Wong <bmw02002turbo@gmail.com>
2020-07-13 21:00:22 -0700

    Removed Unnecessary imports

--- human-benchmark-chimp-test-clicker.py
+++ human-benchmark-chimp-test-clicker.py
@@ -1,12 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 driver.get("https://humanbenchmark.com/tests/chimp")
-# box = driver.find_element_by_xpath("/html/body/div/div/div[4]/div[1]")
-# box.click()
 
 try:
     while True:
@@ -16,10 +11,10 @@ try:
         }
         for counter in range(1, len(newboxes) + 1):
             newboxes[str(counter)].click()
-        try:
-            driver.find_element_by_xpath("/html/body/div/div/div[4]/div[1]/div/div[1]/div[3]/button").click()
-        except:
-            continue
+        # try:
+        #     driver.find_element_by_class_name("css-qm0ri0").click()
+        # except:
+        #     continue
 
 finally:
     driver.quit
