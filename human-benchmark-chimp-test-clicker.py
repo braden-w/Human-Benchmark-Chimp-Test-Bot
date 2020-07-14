311bcc446b10e57ed673cb5203ac05b866d8853a
Braden Wong <bmw02002turbo@gmail.com>
2020-07-13 21:02:25 -0700

    Added comments describing process

--- human-benchmark-chimp-test-clicker.py
+++ human-benchmark-chimp-test-clicker.py
@@ -5,10 +5,12 @@ driver.get("https://humanbenchmark.com/tests/chimp")
 
 try:
     while True:
+        # Create dictionary of <box number>:<box location> before they dissappear
         newboxes = {
             element.text: element
             for element in driver.find_elements_by_class_name("css-19b5rdt")
         }
+        # Use box number key in dictionary as lookup from 1 to n
         for counter in range(1, len(newboxes) + 1):
             newboxes[str(counter)].click()
         # try:
